2e0c005951a8e416ce6acd7ffb03d76a22c176d7
Keshav Gupta <info@keshlabs.in>
2018-07-24 12:52:30 -0400

    wokring #2

--- ai.py
+++ ai.py
@@ -6,76 +6,80 @@ from time import sleep
 class MineSweeper:
     def __init__(self, board):
         self.board = board
-        self.bombSquares = set()
         self.safeSquares = set()
-        self.lastUncovered = set()
+        self.bombSquares = set()
+        self.uncovered = set()
+        self.allSquares = {(x, y) for x in range(self.board.width) for y in range(self.board.height)}
         self.boundary = set()
-        self.newBoundary = set()
-        self.coveredSquares = {(x, y) for x in range(self.board.width) for y in range(self.board.height)}
 
-    def getFlagNeighbors(self, x, y):
-        flagNeighbors = set()
+    def getBombNeighbors(self, (x, y)):
+        s = set()
         for (dx, dy) in [(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, 0), (1, -1), (1, 1)]:
-            (x_, y_) = (x + dx, y + dy)
-            if 0 <= x_ < self.board.width and 0 <= y_ < self.board.height and (x_, y_) in self.bombSquares and self.board.board2D[y_][x_] is None:
-                flagNeighbors.add((x_, y_))
-        return flagNeighbors
+            x_ = x + dx
+            y_ = y + dy
+            if 0 <= x_ < self.board.width and 0 <= y_ < self.board.height and self.board.board2D[y_][x_] is None and (x_, y_) in self.bombSquares:
+                s.add((x_, y_))
+        return s
 
-    def getNonFlagNeighbors(self, x, y):
-        nonFlagNeighbors = set()
+    def getNonBombNeighbors(self, (x, y)):
+        s = set()
         for (dx, dy) in [(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, 0), (1, -1), (1, 1)]:
-            (x_, y_) = (x + dx, y + dy)
-            if 0 <= x_ < self.board.width and 0 <= y_ < self.board.height and (x_, y_) not in self.bombSquares and self.board.board2D[y_][x_] is None:
-                nonFlagNeighbors.add((x_, y_))
-        return nonFlagNeighbors
+            x_ = x + dx
+            y_ = y + dy
+            if 0 <= x_ < self.board.width and 0 <= y_ < self.board.height and self.board.board2D[y_][x_] is None and (x_, y_) not in self.bombSquares:
+                s.add((x_, y_))
+        return s
 
-    def get9Squares(self, x, y):
-        allNeighbors = set()
-        for (dx, dy) in [(0, 1), (-1, 1), (-1, 0), (0, 0), (-1, -1), (0, -1), (1, 0), (1, -1), (1, 1)]:
-            (x_, y_) = (x + dx, y + dy)
+    def get9Block(self, (x, y)):
+        s = set()
+        for (dx, dy) in [(0, 1), (-1, 1), (0, 0), (-1, 0), (-1, -1), (0, -1), (1, 0), (1, -1), (1, 1)]:
+            x_ = x + dx
+            y_ = y + dy
             if 0 <= x_ < self.board.width and 0 <= y_ < self.board.height:
-                allNeighbors.add((x_, y_))
-        return allNeighbors
+                s.add((x_, y_))
+        return s
 
-    def step(self, iterations = 1):
+    def get8Block(self, (x, y)):
+        s = set()
+        for (dx, dy) in [(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, 0), (1, -1), (1, 1)]:
+            x_ = x + dx
+            y_ = y + dy
+            if 0 <= x_ < self.board.width and 0 <= y_ < self.board.height:
+                s.add((x_, y_))
+        return s
+
+    def step(self, maxBorderSize = 15):
         self.board.update2DBoard()
-        boundary = set()
-        for (x, y) in self.lastUncovered:
-            boundary |= self.get9Squares(x, y)
-        boundary -= self.lastUncovered
-        self.newBoundary = set()
-        for (x, y) in boundary:
-            self.newBoundary |= self.get9Squares(x, y)
-        self.newBoundary &= self.lastUncovered
-        self.boundary |= self.newBoundary
-        for _ in range(iterations):
-            for (x, y) in self.boundary:
-                val = self.board.board2D[y][x]
-                flagNeighbors = self.getFlagNeighbors(x, y)
-                nonFlagNeighbors = self.getNonFlagNeighbors(x, y)
-                if val == len(nonFlagNeighbors) + len(flagNeighbors):
-                    for (x_, y_) in nonFlagNeighbors:
+        for (x, y) in self.uncovered:
+            val = self.board.board2D[y][x]
+            if val != 0:
+                bombNeighbors = self.getBombNeighbors((x, y))
+                nonBombNeighbors = self.getNonBombNeighbors((x, y))
+                if val == len(bombNeighbors) + len(nonBombNeighbors):
+                    for (x_, y_) in nonBombNeighbors:
                         self.bombSquares.add((x_, y_))
-                if val == len(flagNeighbors):
-                    for (x_, y_) in nonFlagNeighbors:
+                elif val == len(bombNeighbors):
+                    for (x_, y_) in nonBombNeighbors:
                         self.safeSquares.add((x_, y_))
         if self.safeSquares:
-            self.lastUncovered = set()
             for (bestX, bestY) in self.safeSquares:
-                self.lastUncovered |= self.board.dig(bestX, bestY)
+                uncovered = self.board.dig(bestX, bestY)
+                if uncovered is not None:
+                    self.uncovered |= uncovered
             self.safeSquares = set()
+            self.boundary = set()
         else:
-            for (bestX, bestY) in self.coveredSquares:
-                break
-            self.lastUncovered = self.board.dig(bestX, bestY)
-        if self.lastUncovered is not None:
-            self.coveredSquares -= self.lastUncovered
+            self.boundary = set()
+            covered = self.allSquares - self.uncovered - self.bombSquares
+            for pos in self.uncovered:
+                self.boundary |= self.get9Block(pos) & covered
+
 
 if __name__ == "__main__":
-    width = 100
-    height = 100
-    bombs = 1000
-    mineSize = 5
+    width = 20
+    height = 20
+    bombs = 40
+    mineSize = 20
     sdlBoard = SDLBoard(width, height, bombs, mineSize)
     minesweeper = MineSweeper(sdlBoard)
     i = 0
@@ -83,15 +87,14 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
-        try:
-            print(eval(y))
-        except:
-            pass
+            elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                pos = pygame.mouse.get_pos()
+                minesweeper.uncovered |= sdlBoard.dig((pos[0]-5)/mineSize, (pos[1]-5)/mineSize)
         minesweeper.step()
         sdlBoard.check()
         i += 1
         if i % 1 == 0:
-            sdlBoard.refreshScreen(minesweeper.bombSquares, minesweeper.safeSquares, minesweeper.newBoundary)
+            sdlBoard.refreshScreen(minesweeper.bombSquares, minesweeper.safeSquares, minesweeper.boundary)
         if sdlBoard.state != "ongoing":
             sdlBoard.updateState()
             sleep(1)
